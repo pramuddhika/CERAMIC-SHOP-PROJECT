bc016044505189ca1376305bea5d4c89c74b39dc
Rasanga Pramuddhika <91723368+pramuddhika@users.noreply.github.com>
2024-12-14 11:18:36 +0530

    change ui colurs

--- client/src/app/layout/TopBar.jsx
+++ client/src/app/layout/TopBar.jsx
@@ -2,7 +2,7 @@ import PropTypes from 'prop-types';
 
 const TopBar = ({topic}) => {
   return (
-      <div className="bg-main w-full">
+      <div className="bg-slate-500 w-full">
       <p className="pt-3 pb-3 pl-3 text-2xl font-bold text-white">{topic}</p>
     </div>
   );
