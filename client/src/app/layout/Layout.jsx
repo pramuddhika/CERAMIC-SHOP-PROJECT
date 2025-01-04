36d589ab66768105f2e0d2d427d49b55b7e91825
Rasanga Pramuddhika <91723368+pramuddhika@users.noreply.github.com>
2025-01-04 16:46:10 +0530

    refresh page bug fixed

--- client/src/app/layout/Layout.jsx
+++ client/src/app/layout/Layout.jsx
@@ -1,14 +1,27 @@
 import { Outlet } from "react-router-dom";
 import SideNav from "./SideNav";
 import TopBar from "./TopBar";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const Layout = () => {
   const [activeTopic, setActiveTopic] = useState("Dashboard");
+
+  useEffect(() => {
+    const savedTopic = localStorage.getItem("activeTopic");
+    if (savedTopic) {
+      setActiveTopic(savedTopic);
+    }
+  }, []);
+
+  const handleSetActiveTopic = (topic) => {
+    setActiveTopic(topic);
+    localStorage.setItem("activeTopic", topic);
+  };
+
   return (
     <div className="h-screen">
       <div className="fixed w-[180px]">
-        <SideNav setActiveTopic={setActiveTopic} />
+        <SideNav setActiveTopic={handleSetActiveTopic} />
       </div>
       <div className="flex flex-col h-screen">
         <div className="fixed mb-10 w-calc ml-[180px]">
